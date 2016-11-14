8e11f66127286f74e2e97578291c684e44b6f9a4
Kawika Bader <ekbader@gmail.com>
2016-11-14 09:24:49 -0700

    Created album and artist model class. Added search results element to search component view.

--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SpotifyService } from '../../services/spotify.service';
+import { Artist } from '../../../Artist';
 
 @Component({
     moduleId: module.id,
@@ -8,6 +9,7 @@ import { SpotifyService } from '../../services/spotify.service';
 })
 export class SearchComponent { 
     searchStr: string;
+    searchRes: Artist[];
 
     constructor(private _spotifyService:SpotifyService) {
 		
@@ -15,7 +17,7 @@ export class SearchComponent {
     
     searchMusic() {
         this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
-            console.log(res.artists.items);
+            this.searchRes = res.artists.items;
 		});
 	}
 }
